1029cd3e7cbe0f3337d972c8c59312dc8a9d95ea
Alex <alex@Alexs-MacBook-Pro-2.local>
2023-11-29 14:55:01 -0600

    Adding city search support, updating sytling

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-// App.js
-import React from 'react';
 import './App.css';
-import WeatherApp from './WeatherApp'; // Import the WeatherApp component
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './HomePage';
+import WeatherApp from './WeatherApp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <WeatherApp /> {/* Use the WeatherApp component */}
-      </header>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/weather" element={<WeatherApp />} />
+        </Routes>
+      </Router>
     </div>
+    
   );
 }
+
 
 export default App;
--- src/WeatherApp.js
+++ src/WeatherApp.js
@@ -1,22 +1,15 @@
 import './WeatherApp.css';
 
 import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+
 
 const WeatherApp = () => {
+    const location = useLocation();
+    const { latitude, longitude } = location.state || {};
     const [weatherData, setWeatherData] = useState({ issuingOffice: '', issuanceTime: '', productText: '' });
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(null);
-
-    // Function to get user's GPS coordinates
-    const getLocation = () => {
-        return new Promise((resolve, reject) => {
-            if (!navigator.geolocation) {
-                reject(new Error('Geolocation is not supported by your browser'));
-            } else {
-                navigator.geolocation.getCurrentPosition(resolve, reject);
-            }
-        });
-    };
 
     // Function to fetch NWS Office using coordinates
     const fetchNWSOffice = async (latitude, longitude) => {
@@ -44,16 +37,17 @@ const WeatherApp = () => {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const location = await getLocation();
-                const cwa = await fetchNWSOffice(location.coords.latitude, location.coords.longitude);
-                // const cwa = await fetchNWSOffice(42.766997269347996, -78.6954896010572)
-                const forecastData = await fetchForecastDiscussion(cwa);
-                setWeatherData({
-                    issuingOffice: forecastData.issuingOffice,
-                    issuanceTime: new Date(forecastData.issuanceTime).toLocaleString(),
-                    productText: forecastData.productText
-                });
-                setIsLoading(false);
+                if (latitude && longitude) {
+                    const cwa = await fetchNWSOffice(latitude, longitude);
+                    // const cwa = await fetchNWSOffice(42.766997269347996, -78.6954896010572)
+                    const forecastData = await fetchForecastDiscussion(cwa);
+                    setWeatherData({
+                        issuingOffice: forecastData.issuingOffice,
+                        issuanceTime: new Date(forecastData.issuanceTime).toLocaleString(),
+                        productText: forecastData.productText
+                    });
+                    setIsLoading(false);
+                }
             } catch (err) {
                 setError(err.message);
                 setIsLoading(false);
@@ -61,23 +55,23 @@ const WeatherApp = () => {
         };
 
         fetchData();
-    }, []);
+    }, [latitude, longitude]);
 
     return (
+    <div className="weather-container">
+        {isLoading ? (
+        <p className="loading-error">Loading...</p>
+        ) : error ? (
+        <p className="loading-error">Error: {error}</p>
+        ) : (
         <div>
-            {isLoading ? (
-                <p>Loading...</p>
-            ) : error ? (
-                <p>Error: {error}</p>
-            ) : (
-                <div>
-                    <h1>Weather Forecast Discussion</h1>
-                    <p><strong>Forecast Office:</strong> {weatherData.issuingOffice}</p>
-                    <p><strong>Last Updated:</strong> {weatherData.issuanceTime}</p>
-                    <pre className="product-text">{weatherData.productText}</pre>
-                </div>
-            )}
+            <h1 className="weather-header">Weather Forecast Discussion</h1>
+            <p className="weather-info"><strong>Forecast Office:</strong> {weatherData.issuingOffice}</p>
+            <p className="weather-info"><strong>Last Updated:</strong> {weatherData.issuanceTime}</p>
+            <pre className="weather-content">{weatherData.productText}</pre>
         </div>
+        )}
+    </div>
     );
 };
 
